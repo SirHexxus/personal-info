3eb20610f23952ef62891f5c826b99264747737f
SirHexxus <jamesmichaelstacy@gmail.com>
2019-12-04 16:56:29 -0800

    updated models to store project and password data

---
+++ models/password.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Password = sequelize.define("Password", {
+    name: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      notNull: true
+    }
+  });
+  return Password;
+};
