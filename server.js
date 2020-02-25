d5036cf86c62b1a5318394380168984491e526c5
SirHexxus <jamesmichaelstacy@gmail.com>
2020-02-24 23:02:25 -0800

    checking env variables

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //  Package Variable Declarations
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
 //  Package Dependent Variable Declarations
 const app = express();
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 //  File Import Variable Declarations
 //  //  Configuration for MongoDB
-const db = require("./config/keys").mongoURI;
+console.log(process.env);
+const db = require('./config/keys').mongoURI;
 
 //  Middleware Method Calls
 //  //  Allows the creation of nested objects from a query string
@@ -20,38 +21,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //  //  Configures MongoDB options to avoid deprication errors
 const configOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 };
 
 //  Production Only Assets
 //  //  Serve static assets if app is in production
-if (process.env.NODE_ENV === "production") {
-  //  //  Set static folder
-  app.use(express.static("client/build"));
-  //  //  GET all relevant information, and send it to where it needs to be
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+if (process.env.NODE_ENV === 'production') {
+	//  //  Set static folder
+	app.use(express.static('client/build'));
+	//  //  GET all relevant information, and send it to where it needs to be
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 // Routes
-app.use("/api/", require("./routes/projectRoutes"));
+app.use('/api/', require('./routes/projectRoutes'));
 
 //  Connections
 //  //  Connecting to MongoDB
 mongoose
-  .connect(db, configOptions)
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(err => console.log(err));
+	.connect(db, configOptions)
+	.then(() => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err));
 
 //  //  Starting the server, syncing our models ------------------------------------/
 app.listen(PORT, () => {
-  console.log(
-    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-    PORT,
-    PORT
-  );
+	console.log(
+		'==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+		PORT,
+		PORT
+	);
 });
 
 //  Exports
