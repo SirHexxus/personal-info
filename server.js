78a8aea10fc02866fd994560ed263d63f6a5d83e
SirHexxus <jamesmichaelstacy@gmail.com>
2020-02-25 12:31:13 -0800

    reset certain values to force server to serve up react

--- server.js
+++ server.js
@@ -12,32 +12,28 @@ const PORT = process.env.PORT || 5000;
 
 //  File Import Variable Declarations
 //  //  Configuration for MongoDB
-const db = require('./config/default.json').mongoURI;
+const db = process.env.MONGODB_URI || require('./config/default.json').mongoURI;
 
 //  Middleware Method Calls
 //  //  Allows the creation of nested objects from a query string
 app.use(express.urlencoded({ extended: true }));
 //  //  Recognizes incoming Request Objects as JSON Objects
 app.use(express.json());
-//  //  Configures MongoDB options to avoid deprication errors
+//  //  Set static folder
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+//  //  Configures MongoDB options to avoid deprecation errors
 const configOptions = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 };
 
-//  Production Only Assets
-//  //  Serve static assets if app is in production
-if (process.env.NODE_ENV === 'production') {
-	//  //  Set static folder
-	app.use(express.static('client/build'));
-	//  //  GET all relevant information, and send it to where it needs to be
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
-
 // Routes
+//	//	api Routes
 app.use('/api', require('./routes/projectRoutes'));
+//  //  static Routes
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 //  Connections
 //  //  Connecting to MongoDB
