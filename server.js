aaf38e1359d5a5e4c7ed2368d152987d14866de5
SirHexxus <jamesmichaelstacy@gmail.com>
2020-02-24 23:08:39 -0800

    refactored the mongoURI

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 5000;
 
 //  File Import Variable Declarations
 //  //  Configuration for MongoDB
-console.log(process.env);
 const db = require('./config/keys').mongoURI;
 
 //  Middleware Method Calls
